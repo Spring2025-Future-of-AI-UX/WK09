e3323da6b39d6302a3b89e4eb8ea21db2a6c99d9
winseywu <ww2157@nyu.edu>
2025-04-02 14:54:09 -0400

    Create audioTranscribe.py
    change names to avoid conflict in naming

---
+++ WhisperEx/audioTranscribe.py
@@ -0,0 +1,15 @@
+import whisper
+
+# Load the Whisper model (Options: "tiny", "base", "small", "medium", "large")
+model = whisper.load_model("tiny")
+
+# Path to the audio file
+audio_file = "D:/Future_of_AI_UX/WK09/audio/sample_audio2.wav"
+
+# Transcribe the audio file
+result = model.transcribe(audio_file)
+
+# Print the transcription
+print("Transcription: ", result["text"])
+
+
